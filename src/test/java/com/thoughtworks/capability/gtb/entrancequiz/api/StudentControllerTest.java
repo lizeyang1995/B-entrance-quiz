f4e97751fccba8e8d017be5f2063570250935d35
lizeyang1995 <424011527@qq.com>
2020-10-28 00:00:40 +0800

    [lizeyang] feat: add a test, add student

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentControllerTest.java
@@ -1,15 +1,19 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Students;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,6 +36,16 @@ public class StudentControllerTest {
     @Test
     void should_grouping_students() throws Exception {
         mockMvc.perform(get("/groups"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void should_add_student() throws Exception {
+        Student student = new Student();
+        student.setName("西施");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(student);
+        mockMvc.perform(post("/student").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 }
