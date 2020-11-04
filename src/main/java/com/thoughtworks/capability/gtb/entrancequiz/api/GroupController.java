2e112b0401d15e7e5c4b045810ccaff3982a00bd
lizeyang1995 <424011527@qq.com>
2020-11-04 17:05:29 +0800

    [lizeyang] feat: create GroupController

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/GroupController.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.capability.gtb.entrancequiz.api;
+
+import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GroupController {
+    private StudentService studentService;
+
+    public GroupController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/groups")
+    public ResponseEntity getRandomGroups() {
+        List<List<Map<String, String>>> groups = studentService.getGroups();
+        return ResponseEntity.ok(groups);
+    }
+}
